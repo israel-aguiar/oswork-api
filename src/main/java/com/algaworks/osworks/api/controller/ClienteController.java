40cbcc6e093f78de8ac752105ac911b3dcd478bd
Israel <israel.aguiar@gmail.com>
2020-04-26 19:07:00 -0300

    Implementações rest para cliente

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,33 +1,85 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.service.CadastroClienteService;
 
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CadastroClienteService cadastroCliente;
+
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("77 99999-4444");
-		cliente1.setEmail("joao@qualquer.com");
 		
+		return clienteRepository.findAll();
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Paulo");
-		cliente2.setTelefone("77 98888-3333");
-		cliente2.setEmail("paulo@qualquer.com");
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
-		return Arrays.asList(cliente1, cliente2);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
 		
+		return ResponseEntity.notFound().build();
+	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return cadastroCliente.salvar(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+			@Valid @RequestBody Cliente cliente) {
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cadastroCliente.salvar(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cadastroCliente.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
